46a13a8e0bd5d294bd2ef6b6df56b500b4148840
rabiaaminu <ahmadrabia929@gmail.com>
2023-12-13 13:43:56 -0800

    all

--- AUTHORS.sh
+++
@@ -1,5 +0,0 @@
-# This file lists all individuals having contributed content to the repository.
-# For how it is generated, see `hack/generate-authors.sh`.
-
-Rabia <ahmadrabia929@gmail.com>
-Chuksmili <chuksmili@gmail.com>
