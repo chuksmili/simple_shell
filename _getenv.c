f4e355736240c2b525a6eca09cd3ca41986b91d1
rabiaaminu <ahmadrabia929@gmail.com>
2023-12-15 00:15:21 -0800

    exit

---
+++ _getenv.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+  * _getenv - Gets an env variable
+  * @name: The variable to find in the system env
+  *
+  * Return: The content of the env variable
+  */
+char *_getenv(const char *name)
+{
+	int i = 0;
+	char *env_var = NULL;
+
+	while (environ[i])
+	{
+		if (_strncmp(name, environ[i], _strlen(name)) == 0)
+		{
+			env_var = _strdup(environ[i]);
+			while (*env_var != '=')
+				env_var++;
+
+			++env_var;
+			return (env_var);
+		}
+		i++;
+	}
+
+	return (NULL);
+}
